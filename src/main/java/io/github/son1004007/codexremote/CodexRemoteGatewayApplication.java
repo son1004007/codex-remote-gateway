d81ca3e614898f4c4418c871120fd16670399368
손기석 <77431668+son1004007@users.noreply.github.com>
2026-08-13 18:16:53 +0900

    feat: add application entrypoint

---
+++ src/main/java/io/github/son1004007/codexremote/CodexRemoteGatewayApplication.java
@@ -0,0 +1,12 @@
+package io.github.son1004007.codexremote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexRemoteGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodexRemoteGatewayApplication.class, args);
+    }
+}
